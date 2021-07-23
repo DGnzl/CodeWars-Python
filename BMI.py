3f236479eda256f47f8521b27295b9b7e8f3a37e
DGnzl <dnlgonzal@outlook.com>
2021-07-22 23:38:18 -0700

    BMI

---
+++ BMI.py
@@ -0,0 +1,13 @@
+def bmi(weight, height):
+    # (bmi = weight / height^2)
+    bmi = weight / (height ** 2)
+    
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25.0:
+        return "Normal"
+    elif bmi <= 30.0:
+        return "Overweight"
+    else:
+        return "Obese"
+bmi(190, 6)
