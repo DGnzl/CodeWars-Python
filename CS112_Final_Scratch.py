622f2c988b5eb88ab13012b19e39a571612ce6dd
DGnzl <dnlgonzal@outlook.com>
2021-12-09 23:57:29 -0800

    Progress on CTF

--- CS112_Final_Scratch.py
+++ CS112_Final_Scratch.py
@@ -1,3 +1,6 @@
+from requests.api import request
+
+
 def First():
     hours_worked = [26, 17, 31, 40, 18, 23, 21, 22, 14, 21, 39, 34, 10, 27, 31, 11, 22, 10, 10, 24]
     pay_rates = [16.35, 24.35, 16.97, 22.9, 24.54, 19.45, 19.51, 16.28, 17.17, 22.82, 20.89, 24.88, 23.43, 15.9, 21.22, 20.19, 24.74, 17.87, 17.24, 18.11]
@@ -44,4 +47,70 @@ def Fourth():
         else:
             ans += c
     print(ans)
-Fourth()
+# Fourth()
+
+def Fifth():
+    import json
+    with open('a1a_financials.json','r') as file:
+        data = json.load(file)
+    ans = [x['date'] for x in data if x['profit'] - (x['num_washes'] * 15) > 5000]
+    print(f"flag{','.join(ans)}")
+# Fifth()
+
+def Nemo_1():
+    input = 'Emmanuel,Cameron,Gracie,Taylor,Aliyah,Cayden,Brady,Henry,Londyn,Emily,Nemo,Eden,Lucas,Jack,Lincoln,Everett,Mya,Sean,Easton,Jaxson'.split(',')
+    start = 7
+    print(input.index('Nemo'))
+# Nemo_1()
+
+def Nemo_2():
+    clue1 = 'Y2FsaWZvcm5pYSBhZHZlbnR1cmU6IGluY3JlZGktY29hc3RlciAxNDIy'
+    clue2 = 'qPbqr o19'
+    print(len(clue1))
+
+def Nemo_3():
+    nums = [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,
+    10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,
+    2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,
+    102334155,165580141,267914296,433494437,701408733,1134903170,1836311903,
+    2971215073,4807526976,7778742049,12586269025,20365011074,32951280099,
+    53316291173,86267571272,139583862445,225851433717,365435296162,591286729879,
+    956722026041,1548008755920,2504730781961,4052739537881,6557470319842,
+    10610209857723,17167680177565,27777890035288,44945570212853,72723460248141,
+    117669030460994,190392490709135,308061521170129,498454011879264,
+    806515533049393,1304969544928657,2111485077978050,3416454622906707,
+    5527939700884757,8944394323791464,14472334024676221,23416728348467685,
+    37889062373143906,61305790721611591,99194853094755497,160500643816367088,
+    259695496911122585,420196140727489673,679891637638612258,1100087778366101931,
+    1779979416004714189,2880067194370816120,4660046610375530309,7540113804746346429,
+    12200160415121876738,19740274219868223167,31940434634990099905,51680708854858323072,
+    83621143489848422977,135301852344706746049,218922995834555169026,354224848179261915075]
+    ans = [x % 25672 for x in nums]
+    print(ans)
+# Nemo_3()
+def Nemo_4():
+    with open('peter_pan_ch1.txt', 'r') as file:
+        input = file.read().lower()
+    idx = 0
+    total = 0
+    while input.find('peter',idx) != -1:
+        total += input.index('peter',idx)
+        idx = min(input.index('peter',idx) + 1, len(input) - 1)
+    print(total)
+Nemo_4()
+
+import requests
+def Moira_2():
+    api_url = 'http://ctf.kronoskoders.com:8888/moria/inside'
+    response = requests.get(api_url)
+    print(type(response.content))
+    # print(response.content)
+# Moira_2()
+
+def Moira_3():
+    api_url = 'http://ctf.kronoskoders.com:8888/moria/door/open'
+    todo = {'Friend': 'mellon'}
+    response = requests.post(api_url, json=todo)
+    print(response.json())
+    print(response.status_code)
+# Moira_3()
