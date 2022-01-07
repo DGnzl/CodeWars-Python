fdeef3d7544e7f4dab4667bf076547029bb589e1
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-01-06 23:34:25 -0800

    Further progress

--- BuyCar.py
+++ BuyCar.py
@@ -1,5 +1,9 @@
 def nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth):
-    # your code
-    return []
+    months = 0
+    savings = 0
+    while (savings + startPriceOld < startPriceNew):
+        months += 1
+        savings += savingperMonth
+    return [savings + startPriceOld - startPriceNew]
 
 print(nbMonths(2000, 8000, 1000, 1.5)) # 6, 766
