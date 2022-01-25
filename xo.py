f45a0aa7be3eaa177be8bcebd23dbe726072ec64
DGnzl <dnlgonzal@outlook.com>
2022-01-24 23:32:38 -0800

    XO Solution

---
+++ xo.py
@@ -0,0 +1,12 @@
+def xo(s):
+    ans = {'x':0, 'o':0}
+    for x in s:
+        if x.lower() in ('x','o'):
+            ans[x.lower()] += 1
+    return ans['x'] == ans['o']
+
+def xo2(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
+
+print(xo2('xxoOOX'))
