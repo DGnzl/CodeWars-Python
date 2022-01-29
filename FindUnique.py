a57af08875d3d6b644a3fa9289e1157f26a44bb6
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-01-28 23:07:09 -0800

    Unique Number solution

---
+++ FindUnique.py
@@ -0,0 +1,11 @@
+def find_uniq(arr):
+    key = 0
+    if (arr[0] == arr[1]):
+        for x in arr:
+            if x != arr[0]:
+                return x
+    else:
+        if arr[0] == arr[2]:
+            return arr[1]
+        else:
+            return arr[0]
