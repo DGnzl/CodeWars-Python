db5134034526b15bd8093221a4e5baf316105cc2
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-03-02 20:59:25 -0800

    Division Count solution

---
+++ DivisionCount.py
@@ -0,0 +1,11 @@
+def divisors(n):
+    ans = 0
+    for x in range(1, n + 1):
+        if n % x == 0:
+            ans += 1
+    # return ans
+    return len([x for x in range(1, n + 1) if n % x == 0])
+
+print(divisors(1))
+print(divisors(4))
+print(divisors(5))
