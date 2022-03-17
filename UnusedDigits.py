95a71f3ef633c6c9eff94b232719fd6032d47964
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-03-16 23:25:56 -0700

    Unused digits solution

--- UnusedDigits.py
+++ UnusedDigits.py
@@ -1,10 +1,4 @@
-def unused_digits(numbers: list[int]):
-    digits = {0,1,2,3,4,5,6,7,8,9}
-    ans = ''
-    for x in numbers:
-        for y in str(x):
-            ans += f'{y}'
-            digits.discard(int(y))
-    print(''.join([str(x) for x in digits]))
+def unused_digits(*args):
+    return "".join(number for number in "0123456789" if number not in str(args))
 
-unused_digits([12, 34, 56, 78])
+print(unused_digits([12, 34, 56, 78]))
